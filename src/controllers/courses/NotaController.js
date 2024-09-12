683cd43e48532dc125d221b1f6ca62a28ea8fff1
erikaliane <erika.ventura@tecsup.edu.pe>
2024-09-11 19:06:38 -0500

    updated-cursos

--- src/controllers/courses/NotaController.js
+++ src/controllers/courses/NotaController.js
@@ -5,16 +5,18 @@ const Module = require('../../models/moduleModel');
 const EvaluationCourse = require('../../models/EvaluationCourseResult');
 const EvaluationModule = require('../../models/EvaluationModuleResult');
 const User = require('../../models/UserModel');
+const CourseStudent = require('../../models/courseStudent');
 const Profile = require('../../models/profileModel');
 const { Op } = require('sequelize');
 const ExcelJS = require('exceljs');
 
 
+
 exports.getCourseGrades = async (req, res) => {
     const { enterpriseId, courseId } = req.params;
-  
+
     try {
-        // Fetch all users by enterprise_id
+        // Fetch all users who are assigned to the specific course
         const users = await User.findAll({
             attributes: ['user_id', 'enterprise_id', 'role_id'],
             where: { enterprise_id: enterpriseId, role_id: 1, is_active: true },
@@ -24,6 +26,12 @@ exports.getCourseGrades = async (req, res) => {
                     attributes: ['first_name', 'last_name'],
                     as: 'userProfile',
                     required: true
+                },
+                {
+                    model: CourseStudent, // Ensure that the user is part of the course
+                    attributes: ['course_id', 'user_id'],
+                    where: { course_id: courseId },
+                    required: true // This makes sure only users enrolled in this course are included
                 },
                 {
                     model: EvaluationCourse, // Course evaluation by user
@@ -52,12 +60,12 @@ exports.getCourseGrades = async (req, res) => {
                 }
             ]
         });
-  
+
         // Process the results to merge module results with the same module_id
         const mergedUsers = users.map(user => {
             const moduleResults = user.ModuleResults.reduce((acc, moduleResult) => {
                 const moduleId = moduleResult.Module.module_id;
-  
+
                 if (!acc[moduleId]) {
                     acc[moduleId] = {
                         module_id: moduleId,
@@ -65,25 +73,25 @@ exports.getCourseGrades = async (req, res) => {
                         results: []
                     };
                 }
-  
+
                 // Push the result with puntaje and created_at into the results array
                 acc[moduleId].results.push({
                     puntaje: moduleResult.puntaje,
                     created_at: moduleResult.created_at
                 });
-  
+
                 return acc;
             }, {});
-  
+
             // Convert the object back to an array and sort each module's results by created_at
             const mergedModuleResults = Object.values(moduleResults).map(module => {
                 module.results.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
                 return module;
             });
-  
+
             // Count the number of complete modules
             const completeModulesCount = mergedModuleResults.length;
-  
+
             // Return the user with the merged and sorted module results and complete module count
             return {
                 ...user.toJSON(),
@@ -91,21 +99,21 @@ exports.getCourseGrades = async (req, res) => {
                 completeModulesCount // Add the count of complete modules
             };
         });
-  
+
         // Sort users by the number of complete modules and then by their highest score
         mergedUsers.sort((a, b) => {
             // First, sort by number of complete modules (descending)
             if (b.completeModulesCount !== a.completeModulesCount) {
                 return b.completeModulesCount - a.completeModulesCount;
             }
-  
+
             // If the number of complete modules is the same, sort by the highest score (descending)
             const maxScoreA = Math.max(...a.ModuleResults.flatMap(module => module.results.map(result => parseFloat(result.puntaje) || 0)));
             const maxScoreB = Math.max(...b.ModuleResults.flatMap(module => module.results.map(result => parseFloat(result.puntaje) || 0)));
-  
+
             return maxScoreB - maxScoreA;
         });
-  
+
         res.json(mergedUsers);
     } catch (error) {
         console.error('Error obteniendo las notas del curso:', error);
@@ -113,6 +121,116 @@ exports.getCourseGrades = async (req, res) => {
     }
 };
 
+
+
+exports.getCourseGradesbyUserId = async (req, res) => {
+    const { enterpriseId, courseId , userId } = req.params;
+
+    try {
+        // Fetch all users who are assigned to the specific course
+        const users = await User.findAll({
+            attributes: ['user_id', 'enterprise_id', 'role_id' ],
+            where: { enterprise_id: enterpriseId, role_id: 1, is_active: true  , user_id : userId},
+            include: [
+                {
+                    model: Profile, // Get user's name
+                    attributes: ['first_name', 'last_name'],
+                    as: 'userProfile',
+                    required: true
+                },
+                {
+                    model: CourseStudent, // Ensure that the user is part of the course
+                    attributes: ['course_id', 'user_id'],
+                    where: { course_id: courseId },
+                    required: true // This makes sure only users enrolled in this course are included
+                },
+                {
+                    model: EvaluationCourse, // Course evaluation by user
+                    attributes: ['course_id', 'puntaje', 'course_result_id', 'created_at'],
+                    order: [['created_at', 'ASC']],
+                    where: { course_id: courseId },
+                    required: false, // Allow user to appear even without course results
+                    include: [
+                        {
+                            model: Course,
+                            attributes: ['course_id', 'name'],
+                        }
+                    ]
+                },
+                {
+                    model: EvaluationModule, // Evaluations for each module
+                    attributes: ['puntaje', 'created_at', 'module_result_id'],
+                    order: [['created_at', 'ASC']], // Order by date
+                    required: false, // Allow user to appear even without module results
+                    include: [
+                        {
+                            model: Module, // Include course modules
+                            attributes: ['name', 'module_id'],
+                        }
+                    ]
+                }
+            ]
+        });
+
+        // Process the results to merge module results with the same module_id
+        const mergedUsers = users.map(user => {
+            const moduleResults = user.ModuleResults.reduce((acc, moduleResult) => {
+                const moduleId = moduleResult.Module.module_id;
+
+                if (!acc[moduleId]) {
+                    acc[moduleId] = {
+                        module_id: moduleId,
+                        module_name: moduleResult.Module.name,
+                        results: []
+                    };
+                }
+
+                // Push the result with puntaje and created_at into the results array
+                acc[moduleId].results.push({
+                    puntaje: moduleResult.puntaje,
+                    created_at: moduleResult.created_at
+                });
+
+                return acc;
+            }, {});
+
+            // Convert the object back to an array and sort each module's results by created_at
+            const mergedModuleResults = Object.values(moduleResults).map(module => {
+                module.results.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+                return module;
+            });
+
+            // Count the number of complete modules
+            const completeModulesCount = mergedModuleResults.length;
+
+            // Return the user with the merged and sorted module results and complete module count
+            return {
+                ...user.toJSON(),
+                ModuleResults: mergedModuleResults,
+                completeModulesCount // Add the count of complete modules
+            };
+        });
+
+        // Sort users by the number of complete modules and then by their highest score
+        mergedUsers.sort((a, b) => {
+            // First, sort by number of complete modules (descending)
+            if (b.completeModulesCount !== a.completeModulesCount) {
+                return b.completeModulesCount - a.completeModulesCount;
+            }
+
+            // If the number of complete modules is the same, sort by the highest score (descending)
+            const maxScoreA = Math.max(...a.ModuleResults.flatMap(module => module.results.map(result => parseFloat(result.puntaje) || 0)));
+            const maxScoreB = Math.max(...b.ModuleResults.flatMap(module => module.results.map(result => parseFloat(result.puntaje) || 0)));
+
+            return maxScoreB - maxScoreA;
+        });
+
+        res.json(mergedUsers);
+    } catch (error) {
+        console.error('Error obteniendo las notas del curso:', error);
+        res.status(500).json({ error: 'Error al obtener las notas del curso' });
+    }
+};
 
 
 exports.downloadCourseGradesExcel = async (req, res) => {
