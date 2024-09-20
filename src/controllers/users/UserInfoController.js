e0eff9e98ccc76c27f05806ceb482b34a1a59b73
erikaliane <erika.ventura@tecsup.edu.pe>
2024-09-20 18:17:56 -0500

    updated-modals

--- src/controllers/users/UserInfoController.js
+++ src/controllers/users/UserInfoController.js
@@ -1,20 +1,30 @@
-// controllers/userInfoController.js
 const UserInfo = require('../../models/UserInfo');
 const awsService = require('../../services/archivos/archivoService');
 const awsServiceI = require('../../services/images/imageService');
 const path = require('path');
 
-exports.createUserInfo = async (req, res) => {
+const createUserInfo = async (req, res) => {
   try {
+    console.log('Request Body:', req.body); 
     const { user_id } = req.body;
-    const { foto_image, firma_image, documento_pdf } = req.files; // Assuming files are sent using 'multipart/form-data'
+    const { foto_image, firma_image, documento_pdf } = req.files; // Archivos subidos usando multer
 
-    // Upload each file to AWS S3 and get URLs
-    const fotoUrl = await awsServiceI.uploadImage(foto_image.path, 'foto_user');
-    const firmaUrl = await awsServiceI.uploadImage(firma_image.path, 'firmas');
-    const documentoUrl = await awsService.uploadPdf(documento_pdf.path, 'documents_user');
+    // Verificar que los archivos existan
+    if (!foto_image || !firma_image || !documento_pdf) {
+      return res.status(400).json({ error: 'Todos los archivos son requeridos' });
+    }
 
-    // Create a new UserInfo record
+    // Acceder a los archivos subidos
+    const fotoPath = foto_image[0].path;
+    const firmaPath = firma_image[0].path;
+    const documentoPath = documento_pdf[0].path;
+
+    // Subir los archivos a S3 y obtener las URLs
+    const fotoUrl = await awsServiceI.uploadImage(fotoPath, 'foto_user');
+    const firmaUrl = await awsServiceI.uploadImage(firmaPath, 'firmas');
+    const documentoUrl = await awsService.uploadPdf(documentoPath, 'documents_user');
+
+    // Crear un nuevo registro de UserInfo en la base de datos
     const newUserInfo = await UserInfo.create({
       user_id,
       foto_image: fotoUrl,
@@ -24,9 +34,40 @@ exports.createUserInfo = async (req, res) => {
 
     res.status(201).json(newUserInfo);
   } catch (error) {
-    console.error(`Error creating UserInfo: ${error}`);
-    res.status(500).json({ error: 'Error creating UserInfo' });
+    console.error(`Error creando UserInfo: ${error}`);
+    res.status(500).json({ error: 'Error creando UserInfo' });
   }
 };
 
-// Other CRUD operations (e.g., update, delete, etc.) can be added similarly
+const shouldShowModal = async (req, res) => {
+  try {
+      const userId = req.user.id; // Asumiendo que obtienes el user_id del token o sesión
+      const today = new Date();
+      today.setHours(0, 0, 0, 0); // Establece la hora a medianoche para comparar solo la fecha
+
+      // Busca si el usuario ya tiene un registro con la fecha de hoy
+      const userInfo = await UserInfo.findOne({
+          where: {
+              user_id: userId,
+              created_at: {
+                  [Op.gte]: today // Verifica si created_at es igual o mayor a la medianoche de hoy
+              }
+          }
+      });
+      if (userInfo) {
+          // Si ya hay un registro de hoy, no mostrar modal
+          return res.status(200).json({ showModal: false });
+      } else {
+          // Si no hay registro de hoy, mostrar modal
+          return res.status(200).json({ showModal: true });
+      }
+  } catch (error) {
+      console.error("Error checking modal status:", error);
+      return res.status(500).json({ error: 'Error en el servidor' });
+  }
+};
+
+module.exports = {
+  createUserInfo,
+  shouldShowModal,
+};
--- src/routes/users/UserInfoRoutes.js
+++ src/routes/users/UserInfoRoutes.js
@@ -7,12 +7,12 @@ const userInfoController = require('../../controllers/users/UserInfoController')
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // Store temporary files before uploading to S3
 
-// Route to create UserInfo (upload image, signature, and PDF)
 router.post('/create', upload.fields([
   { name: 'foto_image', maxCount: 1 },
   { name: 'firma_image', maxCount: 1 },
   { name: 'documento_pdf', maxCount: 1 }
 ]), userInfoController.createUserInfo);
 
+router.get('/modals', userInfoController.shouldShowModal);
 
 module.exports = router;
